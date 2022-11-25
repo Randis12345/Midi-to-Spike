9b3ffabb735eff279e115a7d10523262592030e7
Anderson Ray <goandersonray@gmail.com>
2022-11-24 22:45:43 -0500

    forgot to commit all modified files from last commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Also not finished
 
 # Usefull links for understanding mido and midi files
 https://music.stackexchange.com/questions/34402/understanding-midi-files
+https://mido.readthedocs.io/en/latest/midi_files.html#about-the-time-attribute
+https://mido.readthedocs.io/en/latest/index.html
+
+seconds = beats*60/bpm
--- midi to spike.py
+++ midi to spike.py
@@ -1,11 +1,10 @@
-import mido
-
+import mido,scamp
 
 TRACK_NUM = 8
 
-mid = mido.MidiFile(r"Queen_-_Bohemian_Rhapsody.mid")
+mid = mido.MidiFile(r"Super Mario 64 - Medley.mid")
 
-mess = [x.dict() for x in mid.tracks[8] if x.dict()["type"] in ["note_off","note_on"]]
+mess = [x.dict() for x in mid.tracks[1] if x.dict()["type"] in ["note_off","note_on"]]
 
 instruct = []#[[x["note"],x["time"]] for x in mess if x["type"] == "note_on"]
 
@@ -18,9 +17,16 @@ for xi,x in enumerate(mess):
                 time_sum+=y["time"]
                 if y ["type"] == "note_on":
                     break
+            if time_sum == 0: time_sum = 1
             instruct.append([x["note"],time_sum])
         else:
             instruct.append([x["note"],1])
 
         
 for x in instruct: print(x)
+
+sess = scamp.Session()
+sess.tempo = 150000
+piano = sess.new_part("paino")
+
+for x in instruct: piano.play_note(x[0],1,x[1])
